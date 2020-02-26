8b56329ab31c963cd81a31960c7bc6994d3c81f5
pengfei.liu <pengfei.liu@amlogic.com>
2020-02-26 15:49:02 +0800

    dvr: optimize segment tell time [1/1]
    PD#SWPL-18817
    
    Problem:
    New feature
    
    Solution:
    optimize segment tell time
    
    Verify:
    Android Q + AC214
    
    Change-Id: Ia594fbd054aade2586a23a4be746ca68b7a7a5d2
    Signed-off-by: pengfei.liu <pengfei.liu@amlogic.com>

--- include/segment.h
+++ include/segment.h
@@ -86,12 +86,19 @@ loff_t segment_seek(Segment_Handle_t handle, uint64_t time);
  */
 loff_t segment_tell_position(Segment_Handle_t handle);
 
-/**\brief Tell the current time for the giving segment
+/**\brief Tell the current time for the giving segment, used for playback
  * \param[in] handle, Segment handle
  * \return The segment current time on success
  * \return error code on failure
  */
-uint64_t segment_tell_time(Segment_Handle_t handle);
+uint64_t segment_tell_current_time(Segment_Handle_t handle);
+
+/**\brief Tell the total time for the giving segment
+ * \param[in] handle, Segment handle
+ * \return The segment total time on success
+ * \return error code on failure
+ */
+uint64_t segment_tell_total_time(Segment_Handle_t handle);
 
 /**\brief Store the segment information to a file
  * \param[in] handle, The segment handle
